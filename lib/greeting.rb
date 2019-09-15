9c74ac2f2e4fab274dcc51d21129f611afbe0c69
Carla Holcomb <carla.programmer@gmail.com>
2019-09-15 05:35:21 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,5 @@
 # code the #greeting method here!
+puts "Hi! Welcome to the wonderful world of Ruby programming."
+puts "Please enter your name so that we can greet you more personally:"
+name = gets.strip
+greeting(name)
